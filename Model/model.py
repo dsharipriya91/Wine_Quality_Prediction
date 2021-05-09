87b96638c7cd233f23d51dfb6541f523ee19ca4e
Haripriya S <ds.haripriya91@gmail.com>
2021-05-09 15:57:57 +0530

    Modified Files Added

--- Model/model.py
+++ Model/model.py
@@ -9,16 +9,26 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 import pickle
 
+#Read the dataset
 df = pd.read_csv('winequalityN.csv')
+#Create Target Variable
 y = [ 1 if x=='white' else 0 for x in df.type] 
-df.drop(['type','citric acid','chlorides','free sulfur dioxide','total sulfur dioxide','density','pH','sulphates','alcohol','quality'],axis=1, inplace = True)
+
+#Keep top 4 important columns
+df.drop(['type', 'fixed acidity', 'citric acid',
+       'residual sugar', 'free sulfur dioxide',
+       'total sulfur dioxide', 'pH', 'sulphates',
+       'quality'],axis=1, inplace = True)
+
+#Fill mean for missing values
 df.update(df.fillna(df.mean()))
 
+#Chosen Random Forest Classifier from experiments
 rnd = RandomForestClassifier()
 rnd.fit(df,y)
 
-
+#Create pickle file
 pickle.dump(rnd,open('model.pkl','wb'))
 
 model = pickle.load(open('model.pkl','rb'))
-print(model.predict([[6.3, 0.34,1.6]]))
+print(model.predict([[6.3, 0.34,1.6, 0.5]]))
--- app.py
+++ app.py
@@ -22,9 +22,9 @@ def predict():
     int_features = [int(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
-    output = 'white' if prediction[0]==1 else 'red'
+    output = 'White Wine' if prediction[0]==1 else 'Red Wine'
     
-    return render_template('index.html', prediction_text='Wine Prediction: {}'.format(output))
+    return render_template('index.html', prediction_text='Wine Prediction Result: {}'.format(output))
 
 if __name__ == '__main__':
     app.run(debug=True)
